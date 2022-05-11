005c600b3aa528c1ecd22dd5bb7efdc896789166
kenny <kennyufa444@gmail.com>
2022-05-11 20:16:48 +0500

    парсер заклинаний днд - старт

--- parser_dnd/parser_dnd.py
+++ parser_dnd/parser_dnd.py
@@ -1,30 +1,26 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-filtered = []
-hrefval = []
-
+component = []
+name = []
+link = []
 
 def dnd_spell_parser():
-    """
-
-    :param day: день в формате '2020-12-25'
-    :return: парсит сайт return {'date': '2020-12-10', 'am_temp': '-21˚', 'pm_temp': '-13˚', 'weather_day': 'Snow'
-    """
-    mysityurl = f'https://dnd.su/spells/'
+    mysityurl = 'https://dnd.su/spells/'
     html = urlopen(mysityurl)
     bs0 = BeautifulSoup(html, 'html.parser', )
     spells = bs0.findAll('li', attrs={'class': "for_filter"})
     for data in spells:
-        # < a href = "/spells/66-shocking_grasp/" title = "Электрошок [Shocking grasp]" > Электрошок < / a >
-        x = data.find('a')
-
-        # ['/spells/1-hellish_rebuke/ - Адское возмездие [Hellish rebuke]',
-        # '/spells/2-antipathy_sympathy/ - Антипатия/симпатия [Antipathy/sympathy]',...]
-        y = hrefval.append(x.get('href') + ' - ' + x.get('title'))
-
-    for i in hrefval:
-        print(i)
+        components = data.findAll('span', attrs={"class": "end_icon_component"})
+        for comp in components:
+            component.append(comp.get('title'))
+        name_and_link = data.find('a')
+        link.append(name_and_link.get('href'))
+        name.append(name_and_link.get('title'))
+        print(link)
+        print(name)
+        print(component)
+        return
 
 
 dnd_spell_parser()
