9d0c3dbfa9e584a720361b57e6ea52df36d38f62
kenny <kennyufa444@gmail.com>
2022-05-11 20:53:51 +0500

    чтение по каталогу заклинаний и чтение заклинаний

--- parser_dnd/parser_dnd.py
+++ parser_dnd/parser_dnd.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 component = []
 name = []
-link = []
+link = ['/spells/1-hellish_rebuke/']
 
 def dnd_spell_parser():
     mysityurl = 'https://dnd.su/spells/'
@@ -22,5 +22,17 @@ def dnd_spell_parser():
         print(component)
         return
 
+def next_link(link):
+    url='https://dnd.su'+link[0]
+    html = urlopen(url)
+    bs0 = BeautifulSoup(html, 'html.parser')
+    info = bs0.find('ul', attrs={'class': "params"})
+    for li in info.find_all("li"):
+        print(li.text, end="\n")
 
-dnd_spell_parser()
+# dnd_spell_parser()
+next_link(link)
+
+
+
+
