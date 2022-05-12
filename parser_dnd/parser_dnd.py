fb73674b65628ad84f8c398636e9717772348cae
kenny <kennyufa444@gmail.com>
2022-05-12 20:28:58 +0500

    переписал логику

--- parser_dnd/parser_dnd.py
+++ parser_dnd/parser_dnd.py
@@ -22,8 +22,8 @@ def dnd_spell_parser():
         total.update({start: spell_id})
         spell_id = {}
         start += 1
-        pprint.pprint(total)
-        sleep(5)
+        print(total)
+        sleep(1)
     return
 
 
@@ -32,31 +32,37 @@ def next_link(link, spell_id):
     html = urlopen(url)
     bs0 = BeautifulSoup(html, 'html.parser')
     info = bs0.find('ul', attrs={'class': "params"})
-    all_li = info.find_all("li")
-    for l in all_li:
-        bufer = l.getText().split(':')
-        if bufer[0][:1].isdigit():
-            i = bufer[0].split(',')
-            spell_id.update({'lvl': i[0].strip()})
-            spell_id.update({'school': i[1].strip()})
-        if bufer[0] == 'Время накладывания':
-            spell_id.update({bufer[0]: bufer[1].strip()})
-        if bufer[0] == 'Дистанция':
-            spell_id.update({bufer[0]: bufer[1].strip()})
-        if bufer[0] == 'Компоненты':
-            spell_id.update({bufer[0]: bufer[1].strip()})
-        if bufer[0] == 'Длительность':
-            spell_id.update({bufer[0]: bufer[1].strip()})
-        if bufer[0] == 'Классы':
-            spell_id.update({bufer[0]: bufer[1].strip()})
-        if bufer[0] == 'Архетипы':
-            spell_id.update({bufer[0]: bufer[1].strip()})
-        if bufer[0] == 'Источник':
-            spell_id.update({bufer[0]: bufer[1].strip()})
-        else:
-            spell_id.update({'Описание': re.sub('[\t\r\n]', '', str(bufer[0]))})
+    all_li = info.find_all("li", recursive=False)
+    # print(all_li)
+    step_one = all_li[0].getText().split(',')
+    spell_id.update({'lvl': step_one[0].strip()})
+    spell_id.update({'school': step_one[1].strip()})
+    step_two = all_li[1].getText().split(':')
+    spell_id.update({step_two[0]: step_two[1].strip()})
+    step_three = all_li[2].getText().split(':')
+    spell_id.update({step_three[0]: step_three[1].strip()})
+    step_four = all_li[3].getText().split(':')
+    spell_id.update({step_four[0]: step_four[1].strip()})
+    step_five = all_li[4].getText().split(':')
+    spell_id.update({step_five[0]: step_five[1].strip()})
+    step_six = all_li[5].getText().split(':')
+    spell_id.update({step_six[0]: step_six[1].strip()})
+    step_seven = all_li[6].getText().split(':')
+    spell_id.update({step_seven[0]: step_seven[1].strip()})
+    if len(all_li) == 8:
+        step_eight = all_li[7].getText()
+        spell_id.update({'Описание': re.sub('[\t\r\n\xa0]', '', str(step_eight))})
+    elif len(all_li) == 9:
+        step_eight = all_li[7].getText().split(':')
+        spell_id.update({step_eight[0]: step_eight[1].strip()})
+        step_eight = all_li[7].getText()
+        spell_id.update({'Описание': re.sub('[\t\r\n\xa0]', '', str(step_eight))})
+    else:
+        print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+        print(len(all_li))
+        print(all_li)
+        return
 
-    return spell_id
-
-
-dnd_spell_parser()
+x = {}
+next_link('/spells/3052-tasha_s_otherworldly_guise/', x)
+print(x)
